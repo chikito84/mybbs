3d918da44620d93d87a67d266a738fa048772d05
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2018-10-10 08:41:44 +0900

    can add the new topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,9 +1,15 @@
 class TopicsController < ApplicationController
   def index
-    # topicsテーブルの全ての行を取ってくる
     @topics = Topic.all
+    @newTopic = Topic.new
   end
 
   def show
+  end
+
+  def create
+    @topic = Topic.new(params[:topic].permit(:title))
+    @topic.save
+    redirect_to topics_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'topics#index'
   get 'topics/index'
   get 'topics/show/:id', to: 'topics#show', as: 'topics_show'
+  post 'topics/create', to: 'topics#create'
 end
