3444838c7b4c4f92a55ddab4107270777b60558e
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2018-10-10 08:51:47 +0900

    can delete the topic

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'topics/index'
   get 'topics/show/:id', to: 'topics#show', as: 'topics_show'
   post 'topics/create', to: 'topics#create'
+  delete 'topics/delete/:id', to: 'topics#delete', as: 'topics_delete'
 end
